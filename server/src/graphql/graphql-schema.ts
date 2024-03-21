fa0171b63fc7c0ab800ada31ef569147158c4c19
69VR69 <44296242+69VR69@users.noreply.github.com>
2024-03-21 10:22:09 +0100

    Add new GraphQL types and queries

--- server/src/graphql/graphql-schema.ts
+++ server/src/graphql/graphql-schema.ts
@@ -1,40 +1,88 @@
 export const typeDefs = `#graphql
-  type Connection {
-  id: ID!
-  technology: String!
-  status: String!
-  residence: Residence!
+  type Query {
+  dysfunctions: [Dysfunction!]!
+  eligibility: [Eligibility!]!
+  copperClosures: [CopperClosure!]!
+  buildings: [Building!]!
+  users: [User!]!
 }
 
-type Residence {
+type Dysfunction {
+  id: ID!
+  date: String!
+  areaAffected: String!
+  subscriptionAffected: [Subscription!]!
+  reason: String!
+  location: Location!
+}
+
+type Eligibility {
+  id: ID!
+  areaId: ID!
+  areaType: String!
+  technologyAvailability: String!
+  speedClass: String!
+  location: Location!
+}
+
+type CopperClosure {
+  id: ID!
+  date: String!
+  areaAffected: String!
+  reason: String!
+  location: Location!
+}
+
+type Location {
   id: ID!
   address: String!
-  connections: [Connection!]!
+  postalCode: String!
+  building: Building
 }
 
-type Notification {
+type Building {
   id: ID!
-  message: String!
-  source: String!
-  timestamp: String!
+  address: String!
+  postalCode: String!
+  broadbandAvailability: Boolean!
+  fiberOpticAvailability: Boolean!
+  users: [User!]!
+  copperClosures: [CopperClosure!]!
 }
 
-type Query {
-  residences: [Residence!]!
-  residence(id: ID!): Residence
-  connections: [Connection!]!
-  connection(id: ID!): Connection
-  notifications: [Notification!]!
-  notification(id: ID!): Notification
+type User {
+  id: ID!
+  name: String!
+  email: String!
+  building: Building!
+  devices: [Device!]!
 }
 
-type Mutation {
-  updateConnectionStatus(id: ID!, status: String!): Connection!
-  sendNotification(message: String!, source: String!, timestamp: String!): Notification!
+type Device {
+  id: ID!
+  name: String!
+  type: String!
+  user: User!
+}
+
+type Service {
+  id: ID!
+  name: String!
+  provider: String!
 }
 
 type Subscription {
-  connectionStatusUpdated: Connection!
-  newNotification: Notification!
+  id: ID!
+  user: User!
+  service: Service!
+}
+
+type Mutation {
+  # Add mutation operations here if needed
+}
+
+schema {
+  query: Query
+  mutation: Mutation
 }
 `;
