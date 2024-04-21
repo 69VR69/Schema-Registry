931f21f2ebdb41974b70098971ccfff5aa1e0a8c
69VR69 <44296242+69VR69@users.noreply.github.com>
2024-04-21 22:02:51 +0200

    Add kafka consumer to client

--- client/src/Pages/IndexPage.tsx
+++ client/src/Pages/IndexPage.tsx
@@ -1,11 +1,14 @@
+import { Consumer } from "kafkajs";
 import Header from "../Components/Header";
 import { useState, useEffect } from "react";
 
-export default function HomePage() {
+
+export default function HomePage({consumer} : {consumer: Consumer}) {
     const [selectSchemaId, setSelectSchemaId] = useState(1);
     const [content, setContent] = useState("");
     const [schemaList, setSchemaList] = useState([]);
     const [messageResult, setMessageResult] = useState({ text: "", type: "" });
+    const [messages, setMessages] = useState<string[]>([]);
 
     const handleFormSubmit = async (e) => {
         e.preventDefault();
@@ -45,12 +48,26 @@ export default function HomePage() {
                 console.error(error);
             }
         };
+
         fetchSchemaList();
-    }, []);
+
+        const kafka = async () => {
+        await consumer.connect()
+        await consumer.subscribe({ topic: '.*' })
+      
+        await consumer.run({
+            eachMessage: async ({ message }: { message: { value: { toString: () => string } } }) => {
+                console.log('Received message', message.value.toString())
+                setMessages([...messages, message.value.toString()]);
+            },
+        })}
+
+        kafka();
+    }, [consumer, messages]);
 
     return (
         <div className="px-10">
-            <Header/>
+            <Header />
             <div className="grid grid-cols-6 mt-10 w-screen gap-10">
                 {/* Schema list */}
                 <div className="col-span-3 bg-fuchsia-100 rounded-md p-5">
@@ -81,8 +98,19 @@ export default function HomePage() {
                 {/* Consumer */}
                 <div className="col-span-3 bg-green-100 p-5">
                     <h1>Consumers</h1>
+                    <div className="grid grid-cols-6">
+                        <div className=" col-span-3">
+                            <h1>Consumer 1</h1>
+                            <div className="bg-white p-5 rounded-md">
+                                    {messages.map((message, index) => (
+                                        <p key={index}>{message}</p>
+                                    ))}
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
-    );  
+    );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,21 +4,32 @@ import './index.css'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import HomePage from './Pages/IndexPage.tsx';
 import config from '../client-config.json'
+import { Kafka } from "kafkajs";
+
 
 const client = new ApolloClient({
   uri: config.GRAPHQL_URL,
   cache: new InMemoryCache(),
 });
 
+
+const clientId = 'darkpioupiou';
+const brokers = ['localhost:2442'];
+const kafka = new Kafka({
+  clientId,
+  brokers
+});
+const consumer = kafka.consumer({ groupId: 'test-group' })
+
 const rootElement = document.getElementById('root');
-if (!rootElement)  throw new Error('Root element not found');
+if (!rootElement) throw new Error('Root element not found');
 
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <HomePage />
+      <HomePage consumer={consumer} />
     </ApolloProvider>
   </React.StrictMode>
 )
